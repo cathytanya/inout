7b1245eb61f5fac4ab2efdc5fd128e64faa5d9e7
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-10-30 12:38:28 -0400

    Updated route details

--- controllers/api/indoor-routes.js
+++ controllers/api/indoor-routes.js
@@ -13,46 +13,46 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Get indoor activity by ID
-router.get('/:id', async (req, res) => {
-    try {
-      const indoorData = await Indoor.findOne({
-        where: { id: req.params.id },
-        include: [Category],
-      });
-      res.status(200).json(indoorData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+// // Get indoor activity by ID
+// router.get('/:id', async (req, res) => {
+//     try {
+//       const indoorData = await Indoor.findOne({
+//         where: { id: req.params.id },
+//         include: [Category],
+//       });
+//       res.status(200).json(indoorData);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
-// Create new indoor activity
-router.post('/', async (req, res) => {
-  /* req.body should look like this...
-    {
-      activity_name: "Watch a movie",
-      categoryIds: [1, 2, 3, 4]
-    }
-  */
-    try {
-        const indoorData = Indoor.create({
-            indoor: req.body.indoor       
-        })
-        if (req.body.categoryIds) {
-            const categoryIdArr = req.body.categoryIds.map((category_id) => {
-              return {
-                indoor_id: indoor.id,
-                category_id,
-              };
-            });
-            return Category.bulkCreate(categoryIdArr);
-          }
-          res.status(200).json(indoorData);
+// // Create new indoor activity
+// router.post('/', async (req, res) => {
+//   /* req.body should look like this...
+//     {
+//       activity_name: "Watch a movie",
+//       categoryIds: [1, 2, 3, 4]
+//     }
+//   */
+//     try {
+//         const indoorData = Indoor.create({
+//             indoor: req.body.indoor       
+//         })
+//         if (req.body.categoryIds) {
+//             const categoryIdArr = req.body.categoryIds.map((category_id) => {
+//               return {
+//                 indoor_id: indoor.id,
+//                 category_id,
+//               };
+//             });
+//             return Category.bulkCreate(categoryIdArr);
+//           }
+//           res.status(200).json(indoorData);
 
-    } catch (err) {
-    res.status(400).json(err);
-  }
-});
+//     } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 
 
