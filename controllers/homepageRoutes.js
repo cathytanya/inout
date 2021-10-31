5b105c00fbae9f769df64d2c3fc9255623a523c8
navjot turna <shamsherturna101@gmail.com>
2021-10-31 13:30:56 -0400

    changes

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -3,53 +3,67 @@ const router = require('express').Router();
 const { User,Indoor,Outdoor,Category} = require('../models');
 const withAuth = require('../util/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  try {
+// router.get('/homepage', withAuth, async (req, res) => {
+//   try {
     
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      // order: [['name', 'ASC']],
-    });
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password'] },
+//       // order: [['name', 'ASC']],
+//     });
 
-    // const users = userData.map((project) => project.get({ plain: true }));
+//     // const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//     res.render('homepage', {
+//       users,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get('/homepage', (req, res) => {
+
+  res.render('homepage');
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+router.get('/',(req,res)=>{
+  res.redirect('/homepage')
+})
 
-  res.render('login');
-});
+router.get('/outdoor/:category_id',async(req,res)=>{
+  console.log(req.params.category_id)
+try{
+const categoryData = await Category.findAll();
+let categories = categoryData.map((category) => category.get({plain:true}));
 
+const indoorData = await Indoor.findAll(
+{
+  where:{category_id:req.params.category_id}
+}
+);
+    let indoors=indoorData.map((user) => user.get({plain:true}));
+
+    res.render('indoor', {indoors,categories})
+
+} catch (err) {
+res.status(500).json(err);
+}
+
+})
 
 // router.get('/', async (req, res) => {
 //   //TODO: Add a comment describing the purpose of the render method
 //     res.render('index');
 //   });
 
-  router.get('/activities', async (req, res) => {
-    console.log('activities')
+  router.get('/outdoor', async (req, res) => {
+    
     console.log(req.query)
     console.log(req.params)
     //TODO: Add a comment describing the purpose of the render method
     try {
-      //in or out functional psuedo code
-    // const inoutData = await InOut.findAll
-    // let  inouts= inoutdata.map((inout)=>inout.get({plain:true}));
-    // const whereObj={}
-    // if(req.query.inout_id){
-    //   whereObj["id"]=req.query.inout_id
-    // }
+    
 
       // user table data
       const userData = await User.findAll();
@@ -63,18 +77,79 @@ router.get('/login', (req, res) => {
       if(req.query.category_id){
         whereObject["id"]=req.query.category_id
       }
-      const indoorData = await Indoor.findAll({where:whereObject});
-      let indoors=indoorData.map((user) => user.get({plain:true}));
+      
       const outdoorData = await Outdoor.findAll({where:whereObject});
       let outdoors = outdoorData.map((user) => user.get({plain:true}));
-  
-      res.render('activities', {users,indoors,outdoors,categories})
+
+      res.render('outdoor', {users,outdoors,categories})
     } catch (err) {
       res.status(500).json(err);
     }
 
       // res.render('activities');
     });
+
+
+    router.get('/indoor/:category_id',async(req,res)=>{
+      console.log(req.params.category_id)
+try{
+  const categoryData = await Category.findAll();
+  let categories = categoryData.map((category) => category.get({plain:true}));
+
+  const indoorData = await Indoor.findAll(
+    {
+      where:{category_id:req.params.category_id}
+    }
+  );
+        let indoors=indoorData.map((user) => user.get({plain:true}));
+
+        res.render('indoor', {indoors,categories})
+
+} catch (err) {
+  res.status(500).json(err);
+}
+
+    })
+
+    router.get('/indoor', async (req, res) => {
+      console.log('indoor')
+      console.log(req.query)
+      console.log(req.params)
+      //TODO: Add a comment describing the purpose of the render method
+      try {
+    
+        // user table data
+        const userData = await User.findAll();
+        let users = userData.map((user) => user.get({plain:true}));
+       
+        // categories table data
+        const categoryData = await Category.findAll();
+        let categories = categoryData.map((category) => category.get({plain:true}));
+      
+        const indoorData = await Indoor.findAll();
+        let indoors=indoorData.map((user) => user.get({plain:true}));
+      
+        res.render('indoor', {users,indoors,categories})
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  
+     
+      });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     router.get('/final', async (req, res) => {
       //TODO: Add a comment describing the purpose of the render method
