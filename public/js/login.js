7e2d834015b3754f23ff93a3ec1a4091280ea683
navjot turna <shamsherturna101@gmail.com>
2021-11-01 11:41:30 -0400

    login

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+let submit=document.querySelector("#submitbtn")
+//fetch to login user and authenticate
+const login = async (event) => {
+    event.preventDefault();
+    let email = document.querySelector("#form2Example17").value
+    //take values from the form
+    let password = document.querySelector("#form2Example27").value
+    if (email && password) {
+        console.log(email, password);
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { "Content-Type": "application/json" }
+            //the body from the form values
+        })
+        if (response.ok) {
+            document.location.replace('/homepage')
+        }
+        else {
+            alert(response.statusText
+            )
+        }
+    }
+}
+
+submit.addEventListener("click", login)
