9fdeff23597fc4243a6e4c095e880305e4216c73
navjot turna <shamsherturna101@gmail.com>
2021-11-01 14:54:27 -0400

    fianl

---
+++ README.md
@@ -0,0 +1,114 @@
+# <In-Or-Out>
+
+## Description
+
+This application has been built collaboratively within a context of a Pandemic. During the pandemic, many of us saw ourselves limited to indoor activities at our homes, which created the challenge of how to spend our time and entertain with less social activity. Also, at present, COVID-19 cases have decreased and restrictions are being lifted. This means businesses are reopening and social gatherings are becoming more common.
+
+As demand for social activities increase, there is the opportunity to help people find or choose what they want to do and reconnect with their friends, families and passions.
+
+1. This web application will allow users to first choose if they want to stay inside or if they want to go outside. 
+2. Once chosen, the application will display a set of activity cards and present the user with preference buttons to filter activites.
+3. Once filtered, the list of activities will be narrowed down. From this short list the user will be able to select an activity by cliking on the card.
+4. Once an activity is selected, the user will be presented with more details about that specific activity.
+5. If logged in, the user will be allowed to comment on the specific activity comments section and to see what other users have commented.
+
+The USER STORY goes as follow
+
+    AS A user
+    I WANT an application that will generate possible ideas of activites
+    SO THAT I can have options of things to do, spending more time doing and enjoying rather than choosing.  
+
+
+## Table of Contents
+
+- [<In-Or-Out>](#in-or-out)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [GitHub](#github)
+  - [Features](#features)
+  - [Tests](#tests)
+
+## Installation
+
+No installation is needed. This application is run in a live webpage.
+
+## Usage
+
+To use this application, go to the deployed webpage by followig this link: [Visit the deployed page]()
+
+![Image of the Landing Page](./public/images/Index-Screenshot.PNG "Landing Page")
+
+Once in the landing page, go to the upper right corner and click on the user button to either sign up or login.
+
+![Image of a Sign up form asking for username, email and password](./public/images/Signup-Screenshot.PNG "Sign up form")
+![Image of a login form asking for username and password](./public/images/Login-Screenshot.PNG "Login form")
+
+After login, click on IN or OUT at the center of the page. This will take you to a set of either indoor or outdoor activities.
+
+![Image of a series of categories to filter results from](./public/images/Activities-filters-Screenshot.PNG "Choose your preferences")
+
+
+Once in the activites page, you can scroll down to see the full list of indoor or outdoor activites (depending on what you selected before) or you can click on the preferences buttons to filter the activities by category.
+
+![Image of a series of cards with information about activities nearby](./public/images/Activities-list-Screenshot.PNG "Choose from the list of activites")
+
+
+If you click on one of the preferences buttons, the list of activities will be narrowed down by your preference.
+
+If you want to know more about a specific activity and join the conversation, click on one of the activity cards and it will take you to a focused info page, where you will be able to view more details and, if logged in, view and write comments regarding that activity.
+
+![Image of a single card with more details about a single activity and with a list of comments from other users](./public/images/Activity-info-Screenshot.PNG "Find out more about the activity")
+
+## Credits
+
+Collaborators:
+- [Gifty](https://github.com/giftilicious)
+- [Cathy](https://github.com/cathytanya)
+- [Shamsher](https://github.com/SSinghcode)
+- [Andrés](https://github.com/aj-pena)
+
+Third-party assets:
+- [Bootstrap](https://getbootstrap.com/)
+- [JQuery](https://jquery.com/)
+
+Reference material:
+- [w3schools](https://www.w3schools.com/)
+- [StackOverflow](https://stackoverflow.com/)
+- [Mozilla Developer Network](https://developer.mozilla.org/en-US/)
+
+## GitHub
+
+If you want to know more details, feel free to [visit the repo](https://github.com/cathytanya/inout.git)
+
+
+## Features
+
+- Bootstrap
+- Google Fonts
+- Node.js
+- File System
+- npm
+- Inquirer
+- Jest
+- HTML
+- CSS
+- JavaScript
+
+## Tests
+
+Route: 
+'public/js/test.js'
+
+Test files: 
+
+- test.js
+
+How to run them: 
+
+Fork the repo to a local folder.
+From the local folder, open the command-line.
+From the command-line (in the root folder of the local folder), run 'npm run test'.
+You must install the npm jest dependency to be able to run the test using the mentioned command. 
--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,177 +1,153 @@
 const router = require('express').Router();
 
-const { User,Indoor,Outdoor,Category} = require('../models');
+const { User, Indoor, Outdoor, Category } = require('../models');
 const withAuth = require('../util/auth');
 
-// router.get('/homepage', withAuth, async (req, res) => {
-//   try {
-    
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       // order: [['name', 'ASC']],
-//     });
-
-//     // const users = userData.map((project) => project.get({ plain: true }));
-
-//     res.render('homepage', {
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/homepage', (req, res) => {
 
   res.render('homepage');
 });
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
   res.redirect('/homepage')
 })
 
-router.get('/outdoor/:category_id',async(req,res)=>{
+router.get('/outdoor/:category_id', async (req, res) => {
   console.log(req.params.category_id)
-try{
-const categoryData = await Category.findAll();
-let categories = categoryData.map((category) => category.get({plain:true}));
+  try {
+    const categoryData = await Category.findAll();
+    let categories = categoryData.map((category) => category.get({ plain: true }));
 
-const indoorData = await Indoor.findAll(
-{
-  where:{category_id:req.params.category_id}
-}
-);
-    let indoors=indoorData.map((user) => user.get({plain:true}));
+    const outdoorData = await Outdoor.findAll(
+      {
+        where: { category_id: req.params.category_id }
+      }
+    );
+    let outdoors = outdoorData.map((user) => user.get({ plain: true }));
 
-    res.render('indoor', {indoors,categories})
+    res.render('outdoor', { outdoors, categories })
 
-} catch (err) {
-res.status(500).json(err);
-}
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 })
 
-// router.get('/', async (req, res) => {
-//   //TODO: Add a comment describing the purpose of the render method
-//     res.render('index');
-//   });
 
-  router.get('/outdoor', async (req, res) => {
-    
-    console.log(req.query)
-    console.log(req.params)
-    //TODO: Add a comment describing the purpose of the render method
-    try {
-    
 
-      // user table data
-      const userData = await User.findAll();
-      let users = userData.map((user) => user.get({plain:true}));
-     
-      // categories table data
-      const categoryData = await Category.findAll();
-      let categories = categoryData.map((category) => category.get({plain:true}));
-      console.log(categories)
-      const whereObject={}
-      if(req.query.category_id){
-        whereObject["id"]=req.query.category_id
-      }
-      
-      const outdoorData = await Outdoor.findAll({where:whereObject});
-      let outdoors = outdoorData.map((user) => user.get({plain:true}));
+router.get('/outdoor', async (req, res) => {
 
-      res.render('outdoor', {users,outdoors,categories})
-    } catch (err) {
-      res.status(500).json(err);
+  console.log(req.query)
+  console.log(req.params)
+  //TODO: Add a comment describing the purpose of the render method
+  try {
+
+
+    // user table data
+    const userData = await User.findAll();
+    let users = userData.map((user) => user.get({ plain: true }));
+
+    // categories table data
+    const categoryData = await Category.findAll();
+    let categories = categoryData.map((category) => category.get({ plain: true }));
+    console.log(categories)
+    const whereObject = {}
+    if (req.query.category_id) {
+      whereObject["id"] = req.query.category_id
     }
 
-      // res.render('activities');
-    });
+    const outdoorData = await Outdoor.findAll({ where: whereObject });
+    let outdoors = outdoorData.map((user) => user.get({ plain: true }));
+
+    res.render('outdoor', { users, outdoors, categories })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  // res.render('activities');
+});
 
 
-    router.get('/indoor/:category_id',async(req,res)=>{
-      console.log(req.params.category_id)
-try{
-  const categoryData = await Category.findAll();
-  let categories = categoryData.map((category) => category.get({plain:true}));
+router.get('/indoor/:category_id', async (req, res) => {
+  console.log(req.params.category_id)
+  try {
+    const categoryData = await Category.findAll();
+    let categories = categoryData.map((category) => category.get({ plain: true }));
 
-  const indoorData = await Indoor.findAll(
-    {
-      where:{category_id:req.params.category_id}
-    }
-  );
-        let indoors=indoorData.map((user) => user.get({plain:true}));
-
-        res.render('indoor', {indoors,categories})
-
-} catch (err) {
-  res.status(500).json(err);
-}
-
-    })
-
-    router.get('/indoor', async (req, res) => {
-      console.log('indoor')
-      console.log(req.query)
-      console.log(req.params)
-      //TODO: Add a comment describing the purpose of the render method
-      try {
-    
-        // user table data
-        const userData = await User.findAll();
-        let users = userData.map((user) => user.get({plain:true}));
-       
-        // categories table data
-        const categoryData = await Category.findAll();
-        let categories = categoryData.map((category) => category.get({plain:true}));
-      
-        const indoorData = await Indoor.findAll();
-        let indoors=indoorData.map((user) => user.get({plain:true}));
-      
-        res.render('indoor', {users,indoors,categories})
-      } catch (err) {
-        res.status(500).json(err);
+    const indoorData = await Indoor.findAll(
+      {
+        where: { category_id: req.params.category_id }
       }
-  
-     
-      });
-  
+    );
+    let indoors = indoorData.map((user) => user.get({ plain: true }));
+
+    res.render('indoor', { indoors, categories })
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+})
+
+router.get('/indoor', async (req, res) => {
+  console.log('indoor')
+  console.log(req.query)
+  console.log(req.params)
+  //TODO: Add a comment describing the purpose of the render method
+  try {
+
+    // user table data
+    const userData = await User.findAll();
+    let users = userData.map((user) => user.get({ plain: true }));
+
+    // categories table data
+    const categoryData = await Category.findAll();
+    let categories = categoryData.map((category) => category.get({ plain: true }));
+
+    const indoorData = await Indoor.findAll();
+    let indoors = indoorData.map((user) => user.get({ plain: true }));
+
+    res.render('indoor', { users, indoors, categories })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+
+});
+
+
+
+
+router.get('/login',async (req,res)=>{
+
+
+
+  res.render('login')
+})
+
+
+router.get('/signup',(req,res)=>{
+
+
+
+  res.render('signup')
+})
 
 
 
 
 
 
+router.get('/final', (req, res) => {
+//look for that outdoor activity using id form parameter
+  res.render('final');
+});
 
 
 
 
 
-
-
-
-    router.get('/final', async (req, res) => {
-      //TODO: Add a comment describing the purpose of the render method
-        res.render('final');
-      });
-      
-    
 module.exports = router;
 
 
-
-
-
-// ///////
-// FIND THE DATA THAT YOU WANT TO RENDER
-// res.render
-
-// try {
-//   const indoorData = await Indoor.findAll({
-//     include: [{ model: Category }],
-//   });
-//   res.status(200).json(indoorData);
-// } catch (err) {
-//   res.status(500).json(err);
-// }
