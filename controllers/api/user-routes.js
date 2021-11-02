e80aaea7253f1b4359176ab83da305d4b599c613
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-11-01 21:36:55 -0400

    logout test

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,15 +49,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
+
 
 
 module.exports = router;
